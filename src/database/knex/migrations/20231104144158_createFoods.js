72931e2523376a7177f1a903d944f2568711bc7c
GbFerrera <124904382+GbFerrera@users.noreply.github.com>
2023-11-18 15:17:14 -0300

    Atualizando tabela Foods no banco de dados

--- src/database/knex/migrations/20231104144158_createFoods.js
+++ src/database/knex/migrations/20231104144158_createFoods.js
@@ -3,10 +3,14 @@
 exports.up = knex => knex.schema.createTable("foods", table => {
 
   table.increments("id");
+
+  table.text("avatar");
   table.text("title");
+  table.text("category");
+  table.integer("ingredients_id").references("id").inTable("ingredients");
+  table.integer("price");
   table.text("description");
-  table.integer("price")
-  table.integer("ingredients_id").references("id").inTable("ingredients")
+ 
   
   table.timestamp("created_at").default(knex.fn.now());
   table.timestamp("updated_at").default(knex.fn.now());
