6f0229fb082b4de5156ac613bece542db9db9ccb
GbFerrera <124904382+GbFerrera@users.noreply.github.com>
2023-12-23 16:51:35 -0300

    resolvendo conflitos de portas

--- src/server.js
+++ src/server.js
@@ -36,5 +36,5 @@ app.use((error, request, response, next) => {
   })
 })
 
-const PORT = process.env.PORT_SERVER
+const PORT = process.env.PORT_SERVER || 3000
 app.listen(PORT, () => console.log(`Server is running on Port ${PORT}`))
